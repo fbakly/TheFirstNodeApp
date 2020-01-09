094c4aad225fe35738b828becfb2e8484dc1ba44
Glen te Hofsté <g.tehofste@live.nl>
2020-01-09 13:52:11 +0100

    Removed some unused imports and added a Javadoc header.
    Also added a gitignore file.

--- app/src/main/java/com/example/app1/DateAxisFormatter.java
+++ app/src/main/java/com/example/app1/DateAxisFormatter.java
@@ -4,7 +4,6 @@ import com.github.mikephil.charting.charts.Chart;
 import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.formatter.ValueFormatter;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 
--- app/src/main/java/com/example/app1/MainActivity.java
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -1,3 +1,10 @@
+/**
+ * @Class: Mainactivity
+ * @Description: ...
+ * @Methods: ...
+ * @Author: Fouad
+ */
+
 package com.example.app1;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,25 +22,12 @@ import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.parceler.Parcels;
-import org.w3c.dom.Text;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
